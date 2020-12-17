27c43dfacd8f4c20309c29b662825586344039bf
Sowmya Billakanti <sowmya.billakanti8@gmail.com>
2020-12-16 18:08:50 -0800

    Combined all files

--- api-server/src/models/clothes/model.js
+++ api-server/src/models/clothes/model.js
@@ -2,12 +2,12 @@
 
 // const mongoose = require('mongoose');
 
-const clothesSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  color: { type: String, required: true },
-  size: { type: String, required: true }
-});
+// const clothesSchema = mongoose.Schema({
+//   name: { type: String, required: true },
+//   color: { type: String, required: true },
+//   size: { type: String, required: true }
+// });
 
-const clothesModel = mongoose.model('clothes', clothesSchema);
+// const clothesModel = mongoose.model('clothes', clothesSchema);
 
-module.exports = clothesModel;
+// module.exports = clothesModel;
